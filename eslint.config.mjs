2d98a4f0b07d1f78821d6912cd7960728266861a
Oh-Somang <somang1006@gmail.com>
2025-11-28 23:00:21 +0900

    05. list products

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,11 @@ const compat = new FlatCompat({
 })
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...compat.extends(
+    'eslint:recommended',
+    'next/core-web-vitals',
+    'next/typescript'
+  ),
   {
     ignores: [
       'node_modules/**',
